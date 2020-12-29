dc01a3157408b3fca7a4d3847e8da53d321e39cc
ariel <arielbark9@gmail.com>
2020-12-29 16:37:21 +0200

    projects

---
+++ BL/BLImp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLAPI;
+using DLAPI;
+
+namespace BL
+{
+    class BLImp : IBL // internal, use only through interface BLApi.
+    {
+
+    }
+}
---
+++ DalApi/DLFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLAPI
+{
+    public static class DLFactory
+    {
+        public static IDL GetDL(string type)
+        {
+            // TODO: implement
+            return null;
+        }
+    }
+}
